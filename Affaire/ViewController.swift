ab23b9d871137c73626ff3d5ad0494360764b34e
JiruSensei <gillespoirot@mac.com>
2019-04-10 07:24:47 +0200

    Initial Commit

---
+++ Affaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Affaire
+//
+//  Created by Gilles Poirot on 10/04/2019.
+//  Copyright © 2019 iJiru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
