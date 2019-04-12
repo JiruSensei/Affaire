93ffddc25fff8d0b1f5f1cba76314737b4b7c606
JiruSensei <gillespoirot@mac.com>
2019-04-12 07:48:59 +0200

    Mon premier commit

--- Affaire/AffaireViewController.swift
+++ Affaire/AffaireViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
+// On hérite de UITableViewController ce qui évite de
+// avoir à déclarer une variable tableView
+// avoir à s'enregistrer comme delegate et datasource
 class AffaireViewController: UITableViewController {
 
+    var itemArray = ["Rdv radio", "Rdv Ruma", "Buy costume"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Callback pour datasource
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // "" est l'identifier que l'on a donnée à la Property cell dans le storyBoard
+        // onglet "Properties" > identifier quand on clic sur
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AffaireItemCell", for: indexPath)
+        
+        // On crée un tableau fictif pour le moment
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
 
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK: more callback
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select row \(itemArray[indexPath.row])")
+        
+        // Juste pour avoir un effet plus sympa 
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cell: UITableViewCell = tableView.cellForRow(at: indexPath)!
+        if cell.accessoryType == .checkmark {
+            cell.accessoryType = .none
+        }
+        else {
+            cell.accessoryType = .checkmark
+        }
+    }
 }
 
